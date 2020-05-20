b9219c0eb7b7bd3fbf8973dd1559d623e39cea38
Jason Cox <jasoncox@mac.com>
2020-05-20 12:36:59 -0400

    Initial Commit

---
+++ ToDo List/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToDo List
+//
+//  Created by Jason Cox on 5/20/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
